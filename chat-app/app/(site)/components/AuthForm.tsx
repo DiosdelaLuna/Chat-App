2b32dfaa34af95a5d7d46d7aa9417f8a263d7c93
DiosdelaLuna <iloveuos123@gmail.com>
2024-09-08 19:10:21 +0200

    chat commit

---
+++ chat-app/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useState, useCallback } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Input from "../../components/input";
+
+type Variant = "LOGIN" | "REGISTER";
+
+function AuthForm() {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+      //Register
+    }
+    if (variant === "LOGIN") {
+      //SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    //NextAuth Social Sign In
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AuthForm;
